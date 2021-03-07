68c4e23b259c497999978c7bf0d54cbb3a157ee3
Nima <nimiologyy@gmail.com>
2021-03-07 11:55:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,233 +1,108 @@
-from __future__ import unicode_literals
-import os
-from youtube_search import YoutubeSearch
-import youtube_dl
-import subprocess
-import sys
-from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy
-import eyed3.id3
-import eyed3
-import lyricsgenius
-import requests
+import youtube
+from detail import Detail
 import telepot
 import time
+import tag
+import detail
 
-token = 'token bot'
+
+token = '1693683886:AAFg1bOmT7f5k40FfKbVezTuIMnC_tLk_xg'
 bot = telepot.Bot(token)
 
-genius = lyricsgenius.Genius('token genius')
-spotifyy = spotipy.Spotify(
-    client_credentials_manager=SpotifyClientCredentials(client_id='client id spotify',
-                                                        client_secret='client secret spotify'))
-
-
-def handle(msg):
+sort = {}
+def START(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
+    bot.sendMessage(-1001427071315,chat_id)
+    def DOWNLOADER(lenk):
+        DETAIL = Detail(lenk)
+        youtube.search(DETAIL.song_name, DETAIL.time_duration,
+                       DETAIL.time_duration1, DETAIL.minutes,
+                       DETAIL.seconds)
+        youtube.Download(lenk)
+
+    def sendtrack(link):
+        DOWNLOADER(link)
+        bot.sendAudio(-1001316788330, open(f'song//{Detail(link).song_name_folder}.mp3', 'rb'),
+                      title=Detail(link).trackname)
+        bot.sendAudio(chat_id, open(f'song//{Detail(link).song_name_folder}.mp3', 'rb'), title=Detail(link).trackname)
+
+    def send(link):
+        DOWNLOADER(link)
+        bot.sendAudio(chat_id, open(f'song//{Detail(link).song_name_folder}.mp3', 'rb'),
+                      title=Detail(link).trackname)
+        bot.sendAudio(-1001316788330, open(f'song//{Detail(link).song_name_folder}.mp3', 'rb'),
+                      title=Detail(link).trackname)
+
+    def albumdownload(link):
+        messagealbum = ""
+        for song in Detail.album(link):
+            messagealbum += song['name'] + " :\n " + song['external_urls']['spotify'] + '\n'
+        bot.sendMessage(chat_id, messagealbum)
+        for album in Detail.album(link):
+            try:
+                send(album['id'])
+            except:
+                bot.sendMessage(chat_id, "somthing went wrong")
+
+
+
+    msg = msg['text']
+
+
 
     if content_type == 'text':
-        def download(lenk, chat_id = chat_id):
-            results = spotifyy.track(lenk)
-            song = results['name']
-            artist = results['artists'][0]['name']
-            song_name = str(song + " " + artist)
-            song_name_folder = str(song + " - " + artist)
-            artistfinder = results['artists']
-            if len(artistfinder) > 1:
-                fetures = "( Ft."
-                for lomi in range(0, len(artistfinder)):
-                    try:
-                        if lomi < len(artistfinder) - 2:
-                            artistft = artistfinder[lomi + 1]['name'] + ", "
-                            fetures += artistft
-                        else:
-                            artistft = artistfinder[lomi + 1]['name'] + ")"
-                            fetures += artistft
-                    except:
-                        pass
-            else:
-                fetures = ""
 
-            millis = results['duration_ms']
-            millis = int(millis)
-            seconds = (millis / 1000) % 60
-            seconds = int(seconds + 1)
-            minutes = (millis / (1000 * 60)) % 60
-            minutes = int(minutes)
-            if seconds >= 10:
-                time_duration = "%d:%d" % (minutes, seconds)
-            else:
-                time_duration = "{0}:0{1}".format(minutes, seconds)
+        if msg[:30]==('https://open.spotify.com/album') :
+            albumdownload(msg)
 
-            trackname = results['name'] + fetures
-            albumartist = results['artists'][0]['name']
-            tracknumber = results['track_number']
-            albumname = results['album']['name']
-            releasedate = results['album']['release_date']
-
-            bot.sendMessage(chat_id, "Song Found : " + song_name +
-                            '\ntrack : ' + trackname +
-                            '\nalbum artist : ' + albumartist +
-                            "\ntrack num : {0}".format(tracknumber) +
-                            "\nAlbum name : " + albumname +
-                            '\nrealese date : ' + releasedate +
-                            '\nduration : {0}'.format(time_duration))
+        elif msg[:30]== ('https://open.spotify.com/track')  :
             try:
-                bot.sendMessage(chat_id, "Searching...")
-                aud = eyed3.load(f"song//{song_name_folder}.mp3")
-                results = spotifyy.track(lenk)
-                try:
-                    songbook = genius.search_song(results['name'], results['artists'][0]['name'])
-                    try:
-                        bot.sendMessage(chat_id, songbook.lyrics)
-                    except:
-                        pass
-                except:
-                    pass
-
-                response = requests.get(results['album']['images'][0]['url'])
-                DIRCOVER = "songpicts//" + trackname + ".png"
-                file = open(DIRCOVER, "wb")
-                file.write(response.content)
-                file.close()
-
-                aud.tag.artist = artist
-                aud.tag.album = albumname
-                aud.tag.album_artist = albumartist
-                aud.tag.title = trackname
-                aud.tag.track_num = tracknumber
-                aud.tag.year = int(releasedate[:4])
-                try:
-                    aud.tag.lyrics.set(songbook.lyrics)
-                except:
-                    pass
-                aud.tag.images.set(3, open(DIRCOVER, 'rb').read(), 'image/png')
-                aud.tag.save()
-                bot.sendMessage(chat_id, "Sending...")
-                print("Sending")
-                bot.sendAudio(chat_id, open(f'song//{song_name_folder}.mp3', 'rb'), title=trackname)
-                print("Finished!")
+                sendtrack(msg)
             except:
-                song_name_final = song_name.replace(" ", "+")
-                pre_url = "https://www.youtube.com/results?search_query="
-                yt_search = pre_url + song_name_final
-                print(yt_search)
+                bot.sendMessage(chat_id, "somthing went wrong")
 
-                results = list(YoutubeSearch(str(song_name)).to_dict())
-                for URLSSS in range(0, len(results)):
-                    if results[URLSSS]["duration"] == time_duration:
-                        LINKASLI = results[URLSSS]['url_suffix']
+        elif msg[:31] == ('https://open.spotify.com/artist'):
+            messagealbum= ""
+            for song in detail.artist(msg):
+                messagealbum += song['name']+" :\n "+song['external_urls']['spotify']+'\n\n'
+            bot.sendMessage(chat_id,messagealbum)
+            for song in detail.artist(msg):
                 try:
-                    print(LINKASLI)
+                    send(song['href'])
                 except:
-                    bot.sendMessage(chat_id, "404 \n NOT FOUND")
+                    bot.sendMessage(chat_id,"somthing went wrong")
 
-                print("Song Found")
-                yt_pre = str("https://www.youtube.com/" + LINKASLI)
-                print(yt_pre)
-                bot.sendMessage(chat_id, "Downloading... ")
+        elif msg == "/start":
+            bot.sendMessage(chat_id,
+                            "Hi \nsend me spotify link and I'll give you music\nor use /single or /album")
 
-                def check():
-                    import importlib
-                    try:
-                        importlib.import_module('youtube_dl')
+        elif msg == "/album":
+            sort[chat_id]='a'
+            bot.sendMessage(chat_id, 'send name and name of artist like this \nAlbum - Name artist')
 
-                    except ModuleNotFoundError:
-                        print('youtube-dl NOT FOUND in this Computer !')
-                        print('The SCRIPT will install youtube-dl python package . . .')
-                        subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'youtube-dl'])
+        elif msg == '/single':
+            sort[chat_id]='single'
+            bot.sendMessage(chat_id,'send name and name of artist like this \nName - Name artist')
 
-                    finally:
-                        globals()['youtube_dl'] = importlib.import_module('youtube_dl')
-                        run()
+        elif msg.find("-")>0:
+            print(sort)
+            if sort[chat_id]=='album':
+                albumdownload(detail.searchalbum(msg))
+                del sort[chat_id]
+            elif sort[chat_id]=='single':
+                send(detail.searchsingle(msg))
+                del sort[chat_id]
 
-                ffmpeg_path = os.getcwd()
-
-                def tag():
-                    results = spotifyy.track(lenk)
-                    try:
-                        songbook = genius.search_song(results['name'], results['artists'][0]['name'])
-                        try:
-                            bot.sendMessage(chat_id, songbook.lyrics)
-                        except:
-                            pass
-                    except:
-                        pass
-
-                    response = requests.get(results['album']['images'][0]['url'])
-                    DIRCOVER = "songpicts//" + trackname + ".png"
-                    file = open(DIRCOVER, "wb")
-                    file.write(response.content)
-                    file.close()
-
-                    aud = eyed3.load(f"song//{song_name_folder}.mp3")
-                    aud.tag.artist = artist
-                    aud.tag.album = albumname
-                    aud.tag.album_artist = albumartist
-                    aud.tag.title = trackname
-                    aud.tag.track_num = tracknumber
-                    aud.tag.year = int(releasedate[:4])
-                    try:
-                        aud.tag.lyrics.set(songbook.lyrics)
-                    except:
-                        pass
-                    aud.tag.images.set(3, open(DIRCOVER, 'rb').read(), 'image/png')
-                    aud.tag.save()
-                    bot.sendMessage(chat_id, "Sending...")
-                    print("Sending")
-                    bot.sendAudio(chat_id, open(f'song//{song_name_folder}.mp3', 'rb'), title=trackname)
-
-                def run():
-                    options = {
-                        # PERMANENT options
-                        'format': 'bestaudio/best',
-                        'ffmpeg_location': f'{ffmpeg_path}/ffmpeg.exe',
-                        'keepvideo': False,
-                        'outtmpl': f'song//{song_name_folder}.*',
-                        'postprocessors': [{
-                            'key': 'FFmpegExtractAudio',
-                            'preferredcodec': 'mp3',
-                            'preferredquality': '320'
-                        }],
-
-                        # (OPTIONAL options)
-                        'noplaylist': True
-                    }
-
-                    with youtube_dl.YoutubeDL(options) as mp3:
-                        mp3.download([yt_pre])
-                        bot.sendMessage(chat_id, "Downloaded.")
-                        tag()
-                        print("Download Completed!")
-
-                if __name__ == '__main__':
-                    check()
-
-        leenk = msg['text']
-
-
-
-
-        if leenk[:30]==('https://open.spotify.com/album') :
-
-            results = spotifyy.album_tracks(leenk)
-            albums = results['items']
-            while results['next']:
-                results = spotifyy.next(results)
-                albums.extend(results['items'])
-
-            for album in albums:
-                download(album['id'])
-        elif leenk[:30]== ('https://open.spotify.com/track')  :
-
-            download(leenk)
+            else :
+                bot.sendMessage(chat_id,"You didn't sort it")
 
         else:
-            bot.sendMessage(chat_id,"Link invalid")
-bot.message_loop(handle)
+            bot.sendMessage(chat_id,'send me link or use /single or /album')
 
+bot.message_loop(START)
 print('Listening ...')
 
 while 1:
     time.sleep(10)
+
