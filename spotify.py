dc038631b4b18724df00cf30680d9cf9500eca8b
Nima <nimiologyy@gmail.com>
2021-06-14 16:40:20 +0430

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -19,14 +19,17 @@ token = 'token bot'
 bot = telepot.Bot(token)
 
 def DOWNLOADMP3(link,chat_id):
+    #Get Meta DAta
     results = spotifyy.track(link)
     song = results['name']
+    print('[Spotify]MetaData Found!')
     artist = results['artists'][0]['name']
     YTSEARCH = str(song + " " + artist)
     artistfinder = results['artists']
     tracknum = results['track_number']
     album = results['album']['name']
     realese_date = int(results['album']['release_date'][:4])
+
     if len(artistfinder) > 1:
         fetures = "( Ft."
         for lomi in range(0, len(artistfinder)):
@@ -60,11 +63,11 @@ def DOWNLOADMP3(link,chat_id):
             if seconds == 10:
                 time_duration2 = "{0}:0{1}".format(minutes, seconds - 1)
                 time_duration3 = "{0}:{1}".format(minutes, seconds + 2)
-            elif seconds<58:
+            elif seconds < 58:
                 time_duration3 = "{0}:{1}".format(minutes, seconds + 2)
                 time_duration2 = "{0}:{1}".format(minutes, seconds - 1)
             elif seconds == 58:
-                time_duration3 = "{0}:0{1}".format(minutes+1, seconds -58)
+                time_duration3 = "{0}:0{1}".format(minutes + 1, seconds - 58)
                 time_duration2 = "{0}:{1}".format(minutes, seconds - 1)
             else:
                 time_duration2 = "{0}:{1}".format(minutes, seconds - 1)
@@ -79,7 +82,7 @@ def DOWNLOADMP3(link,chat_id):
         if seconds < 8:
             time_duration3 = "{0}:0{1}".format(minutes, seconds + 2)
             time_duration2 = "{0}:0{1}".format(minutes, seconds - 1)
-        elif seconds ==9 or seconds==8:
+        elif seconds == 9 or seconds == 8:
             time_duration3 = "{0}:{1}".format(minutes, seconds + 2)
 
         elif seconds == 0:
@@ -90,14 +93,13 @@ def DOWNLOADMP3(link,chat_id):
             time_duration3 = "{0}:0{1}".format(minutes, seconds + 2)
 
     trackname = song + fetures
-
+    #Download Cover
     response = requests.get(results['album']['images'][0]['url'])
     DIRCOVER = "songpicts//" + trackname + ".png"
     file = open(DIRCOVER, "wb")
     file.write(response.content)
     file.close()
-
-
+    #search for music on youtube
     results = list(YoutubeSearch(str(YTSEARCH)).to_dict())
     LINKASLI = ''
     for URLSSS in results:
@@ -111,8 +113,9 @@ def DOWNLOADMP3(link,chat_id):
             break
 
     YTLINK = str("https://www.youtube.com/" + LINKASLI)
-    print(YTLINK)
-
+    print('[Youtube]song found!')
+    print(f'[Youtube]Link song on youtube : {YTLINK}')
+    #Donwload Music from youtube
     options = {
         # PERMANENT options
         'format': 'bestaudio/best',
@@ -122,17 +125,14 @@ def DOWNLOADMP3(link,chat_id):
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '320'
-        }],
-
-        # (OPTIONAL options)
-        'noplaylist': True
+        }]
     }
 
     with youtube_dl.YoutubeDL(options) as mp3:
         mp3.download([YTLINK])
 
-
     aud = eyed3.load(f"song//{trackname}.mp3")
+    print('[Youtube]Song Downloaded!')
     aud.tag.artist = artist
     aud.tag.album = album
     aud.tag.album_artist = artist
@@ -142,12 +142,13 @@ def DOWNLOADMP3(link,chat_id):
     try:
         songok = genius.search_song(song, artist)
         aud.tag.lyrics.set(songok.lyrics)
+        print('[Genius]Song lyric Found!')
     except:
-        pass
+        print('[Genius]Song lyric NOT Found!')
     aud.tag.images.set(3, open("songpicts//" + trackname + ".png", 'rb').read(), 'image/png')
     aud.tag.save()
-    bot.sendAudio(chat_id, open(f'song//{trackname}.mp3', 'rb'),title=trackname)
-
+    bot.sendAudio(chat_id, open(f'song//{trackname}.mp3', 'rb'), title=trackname)
+    print('[Telegram]Song sent!')
 
 
 def album(link):
@@ -156,33 +157,34 @@ def album(link):
     while results['next']:
         results = spotifyy.next(results)
         albums.extend(results['items'])
+
+    print('[Spotify]Album Found!')
     return albums
 
 
 def artist(link):
     results = spotifyy.artist_top_tracks(link)
     albums = results['tracks']
+    print('[Spotify]Artist Found!')
     return albums
 
 
 def searchalbum(track):
-
     results = spotifyy.search(track)
     return results['tracks']['items'][0]['album']['external_urls']['spotify']
 
 def playlist(link):
     results = spotifyy.playlist_tracks(link)
+    print('[Spotify]Playlist Found!')
     return results['items']
 
 
 def searchsingle(track):
-
     results = spotifyy.search(track)
     return results['tracks']['items'][0]['href']
 
 
 def searchartist(searchstr):
-
     results = spotifyy.search(searchstr)
     return results['tracks']['items'][0]['artists'][0]["external_urls"]['spotify']
 
