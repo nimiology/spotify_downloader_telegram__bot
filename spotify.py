68895f4a6160b84a212d1158b4854cd29e8bbac3
GkVaJyB <89921421+GkVaJyB@users.noreply.github.com>
2021-09-13 21:26:11 +0530

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -22,7 +22,7 @@ else:
 
 bot = telepot.Bot(token)
 
-def DOWNLOADMP3(link,chat_id,):
+def DOWNLOADMP3(link,chat_id):
     #Get MetaData
     results = spotifyy.track(link)
     song = results['name']
