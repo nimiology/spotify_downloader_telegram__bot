2c166ad4320df6091fb87ea14826f77b13fdf572
nima <nimiologyy@gmail.com>
2025-05-30 02:44:51 +0330

    requirements

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -3,7 +3,7 @@ import spotipy
 from decouple import config
 
 SPOTIFY = spotipy.Spotify(
-    client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials(client_id=config("SPOTIPY_CLIENT_ID"),
-                                                                       client_secret=config("SPOTIPY_CLIENT_SECRET")))
+    client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials(client_id=config("SPOTIFY_CLIENT_ID"),
+                                                                       client_secret=config("SPOTIFY_CLIENT_SECRET")))
 
 GENIUS = lyricsgenius.Genius(config("GENIUS_ACCESS_TOKEN"))
